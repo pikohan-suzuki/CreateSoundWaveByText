a91d6e69c9f00b52dc2363ee254470e437d3a751
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2020-09-22 22:09:27 +0900

    add set id func

--- record_voice_android_app/app/src/main/java/com/amebaownd/pikohan_niwatori/recordsoundandtext/MainViewModel.kt
+++ record_voice_android_app/app/src/main/java/com/amebaownd/pikohan_niwatori/recordsoundandtext/MainViewModel.kt
@@ -1,7 +1,12 @@
-package com.amebaownd.pikohan_niwatori.recordsoundandtext.ui.home
+package com.amebaownd.pikohan_niwatori.recordsoundandtext
 
 import androidx.lifecycle.ViewModel
 
 class MainViewModel() : ViewModel() {
+    var id = ""
+        private set
 
+    fun setID(id:String){
+        this.id = id
+    }
 }
