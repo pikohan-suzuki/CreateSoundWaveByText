6b4af91e07ab53d6881df519309da69e6b01afb5
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2020-09-22 22:08:11 +0900

    add constatns

---
+++ record_voice_android_app/app/src/main/java/com/amebaownd/pikohan_niwatori/recordsoundandtext/data/Constants.kt
@@ -0,0 +1,9 @@
+package com.amebaownd.pikohan_niwatori.recordsoundandtext.data
+
+object Constants {
+    const val SAMPLING_RATE = 44100
+    const val FRAME_RATE = 10
+    const val ONE_FRAME_DATA_COUNT = SAMPLING_RATE / FRAME_RATE
+
+    const val REQUEST_CODE = 101
+}
